c5e0ee07dc85d5bd223409b12ee5a7934637ee84
vandadvafai <vafai.vandad@gmail.com>
2025-03-08 22:45:14 +0100

    implementing ETL process

--- src/api_wrapper.py
+++ src/api_wrapper.py
@@ -1,8 +1,22 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
+import simfin as sf
 
-load_dotenv()  
+load_dotenv() 
 
-SIMFIN_API_KEY = os.getenv("SIMFIN_API_KEY")
-if SIMFIN_API_KEY is None:
-    raise ValueError("No SimFin API key found in environment variables!")
+# Set your API-key for downloading data.
+sf.set_api_key('160f0cdb-b61d-4080-b6b1-0a41e0fe7a32')
+
+# Set the local directory where data-files are stored.
+# The directory will be created if it does not already exist.
+sf.set_data_dir('~/simfin_data/')
+
+# Download the data from the SimFin server and load into a Pandas DataFrame.
+prices_df = sf.load_shareprices(variant='daily')
+
+# Download the data from the SimFin server and load into a Pandas DataFrame.
+companies_df = sf.load_companies(market='us')
+
+# Print the first rows of the data.
+print(prices_df.head())
+print(companies_df.head())
