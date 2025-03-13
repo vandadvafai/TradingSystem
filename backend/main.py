9f8c36c8416c588198033641e563f5baa3521fbe
vandadvafai <vafai.vandad@gmail.com>
2025-03-13 17:31:08 +0100

    Track large file using Git LFS

--- backend/main.py
+++
@@ -1,7 +0,0 @@
-# backend/main.py
-
-# A simple placeholder for your backend logic
-# Eventually, you'll add classes/functions for data processing, ML model, etc.
-
-def hello_backend():
-    return "Hello from the Backend!"
